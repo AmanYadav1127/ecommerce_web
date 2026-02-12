90771238c2e7e88439a073ac20748a1e2d56c082
Aman Yadav <aman.yadav.st.johns@gmail.com>
2026-02-12 18:02:11 +0530

    Implemented DTO in update and delete Categories

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -6,25 +6,31 @@ import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class CategoryController {
+    @Autowired
     private CategoryService categoryService;
-private ModelMapper modelMapper;
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
+//private ModelMapper modelMapper;
+//    public CategoryController(CategoryService categoryService) {
+//        this.categoryService = categoryService;
+//    }
+
+//    @GetMapping("/echo")
+//    public ResponseEntity<String>echoMessage(@RequestParam(name = "message",required = false) String message){
+//        return new ResponseEntity<>("Echoed message:"+message, HttpStatus.OK);
+//    }
 
     @GetMapping("/api/public/categories")
-    public ResponseEntity<CategoryResponse> getAllCategory(){
-        CategoryResponse categoryResponse=categoryService.getAllCategories();
+    public ResponseEntity<CategoryResponse> getAllCategory(@RequestParam(name="pageNumber")Integer pageNumber,
+                                                           @RequestParam(name="pageSize")Integer pageSize)
+    {
+        CategoryResponse categoryResponse=categoryService.getAllCategories(pageNumber,pageSize);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
     @PostMapping("/api/public/categories")
@@ -40,8 +46,6 @@ private ModelMapper modelMapper;
     }
     @PutMapping("/api/public/categories/{categoryId}")
     public ResponseEntity<CategoryDto> updateCategory(@Valid @RequestBody CategoryDto categoryDto, @PathVariable Long categoryId){
-//            Category category=modelMapper.map(categoryDto, Category.class);
-//            category.setCategoryId(categoryId);
             CategoryDto savedCategoryDto=categoryService.updateCategory(categoryDto, categoryId);
             return new ResponseEntity<>(savedCategoryDto, HttpStatus.OK);
     }
