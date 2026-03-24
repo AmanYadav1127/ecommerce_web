658a92fc2dac732693a605bcc7baa161dfcc720e
Aman Yadav <aman.yadav.st.johns@gmail.com>
2026-03-25 01:59:04 +0530

    Setting up DTOS

---
+++ src/main/java/com/ecommerce/project/payload/OrderItemDTO.java
@@ -0,0 +1,17 @@
+package com.ecommerce.project.payload;
+
+import com.ecommerce.project.model.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemDTO {
+    private Long orderId;
+    private Product product;
+    private int quantity;
+    private Double discount;
+    private Double orderedProductPrice;
+}
